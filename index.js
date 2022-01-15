18cf2052b9f15e0e0fcc35860a4e51def8291b60
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-01-15 15:01:18 -0500

    completed prompts and HTML template

--- index.js
+++ index.js
@@ -73,7 +73,108 @@ const managerPrompts = () => {
 };
 
 const employeePrompts = () => {
+  return inquirer.prompt ([
+    {
+      type: 'list',
+      name: 'role',
+      message: "What is your employee's role?",
+      choices: ['Engineer', 'Intern']
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is your employee's name?",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log ("Please provide your employee's name.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is your employee's ID number?",
+      validate: nameInput => {
+        if (isNaN(nameInput)) {
+          console.log("Please enter the employee's ID number.")
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the employee's email address?",
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("Please provide the employee's email.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'githubName',
+      message: "What is your engineer's GitHub username?",
+      when: (input) => input.role === "Engineer",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log ("Please provide your engineer's GitHub username.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'schoolName',
+      message: "What school does your intern attend?",
+      when: (input) => input.role === "Intern",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log ("Please provide your intern's school.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'addEmployees',
+      message: 'Would you like to add to your team?',
+      default: 'false'
+    }
+  ])
 
+  .then(teamMembers => {
+    let { name, id, email, role, githubName, schoolName, addEmployees } = teamMembers;
+    let employee;
+
+    if (role === "Engineer") {
+      employee = new Engineer (name, id, email, githubName);
+      console.log(employee);
+    } else if (role === "Intern") {
+      employee = new Intern (name, id, email, schoolName);
+      console.log(employee);
+    }
+
+    teamArr.push(employee);
+
+    if (addEmployees) {
+      return employeePrompts(teamArr)
+    } else {
+      return teamArr;
+    }
+  })
 };
 
 const writeFile = data => {
