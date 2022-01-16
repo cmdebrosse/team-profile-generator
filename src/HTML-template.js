d9623177e2d2d43cd6dc22577ff27bd46a5963a1
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-01-16 15:19:01 -0500

    template finished and styled

--- src/HTML-template.js
+++ src/HTML-template.js
@@ -1,6 +1,84 @@
-module.exports = HTMLData => {
-  const {} = HTMLData;
+const managerCard = function (manager) {
+  return `
+      <div class="col-4 mt-4">
+        <div class="card h-100 shadow p-3 m-3 mb-5 bg-body rounded" style="width: 18rem;">
+          <div class="card-body">
+            <span class="material-icons d-inline">work_outline</span>
+            <p class="card-title d-inline lead">${manager.name}</p>
+            <p class="card-subtitle text-muted">Manager</p>
+            <p class="card-text">ID: ${manager.id}</p>
+            <p class="card-text">Office: ${manager.officeNum}</p>
+            <p class="card-text">Email: <a href="mailto:${manager.email}" class="card-link">${manager.email}</a></p>
+          </div>
+        </div>
+      </div>`
+};
 
+const engineerCard = function (engineer) {
+  return `
+      <div class="col-4 mt-4">
+        <div class="card h-100 shadow p-3 m-3 mb-5 bg-body rounded" style="width: 18rem;">
+          <div class="card-body">
+            <span class="material-icons d-inline">engineering</span>
+            <p class="card-title d-inline lead">${engineer.name}</p>
+            <p class="card-subtitle text-muted">Engineer</p>
+            <p class="card-text">ID: ${engineer.id}</p>
+            <p class="card-text">GitHub Account: <a href="${engineer.githubName}" class="card-link">${engineer.githubName}</a></p>
+            <p class="card-text">Email: <a href="mailto:${engineer.email}" class="card-link">${engineer.email}</a></p>
+          </div>
+        </div>
+      </div>`
+};
+
+const internCard = function (intern) {
+  return `
+      <div class="col-4 mt-4">
+        <div class="card h-100 shadow p-3 m-3 mb-5 bg-body rounded" style="width: 18rem;">
+          <div class="card-body">
+            <span class="material-icons d-inline">laptop</span>
+            <p class="card-title d-inline lead">${intern.name}</p>
+            <p class="card-subtitle text-muted">Intern</p>
+            <p class="card-text">ID: ${intern.id}</p>
+            <p class="card-text">School: ${intern.schoolName}</p>
+            <p class="card-text">Email: <a href="mailto:${intern.email}" class="card-link">${intern.email}</a></p>
+          </div>
+        </div>
+      </div>`
+};
+
+newHTML = (data) => {
+  pageArr = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const employee = data[i];
+    const role = employee.getRole();
+
+    if (role === 'Manager') {
+      const newManager = managerCard(employee);
+
+      pageArr.push(newManager);
+    }
+
+    if (role === 'Engineer') {
+      const newEngineer = engineerCard(employee);
+
+      pageArr.push(newEngineer);
+    }
+
+    if (role === 'Intern') {
+      const newIntern = internCard(employee);
+
+      pageArr.push(newIntern);
+    }
+  }
+
+const teamCards = pageArr.join('');
+
+const generateTeam = generateHTML(teamCards);
+return generateTeam;
+};
+
+const generateHTML = function (teamCards) {
   return `<!DOCTYPE html>
   <html lang="en">
   
@@ -23,41 +101,10 @@ module.exports = HTMLData => {
     </header>
   
     <div class="container">
-      <div class="card-group" id="team-cards">
-  
-        <div class="card shadow p-3 m-3 mb-5 bg-body rounded" style="width: 18rem;">
-          <div class="card-body">
-            <span class="material-icons d-inline">work_outline</span>
-            <p class="card-title d-inline lead">Chuck DeBrosse</p>
-            <p class="card-subtitle text-muted">Manager</p>
-            <p class="card-text">ID: 111</p>
-            <p class="card-text">Office Number: 1</p>
-            <p class="card-text">Email: <a href="mailto:cmdebrosse@outlook.com" class="card-link">cmdebrosse@outlook.com</a></p>
-          </div>
-        </div>
-  
-        <div class="card shadow p-3 m-3 mb-5 bg-body rounded" style="width: 18rem;">
-          <div class="card-body">
-            <span class="material-icons d-inline">engineering</span>
-            <p class="card-title d-inline lead">Chuck DeBrosse</p>
-            <p class="card-subtitle text-muted">Engineer</p>
-            <p class="card-text">ID: 222</p>
-            <p class="card-text">GitHub Account: <a href="https://github.com/cmdebrosse" class="card-link">cmdebrosse</a></p>
-            <p class="card-text">Email: <a href="mailto:cmdebrosse@outlook.com" class="card-link">cmdebrosse@outlook.com</a></p>
-          </div>
-        </div>
-  
-        <div class="card shadow p-3 m-3 mb-5 bg-body rounded" style="width: 18rem;">
-          <div class="card-body">
-            <span class="material-icons d-inline">laptop</span>
-            <p class="card-title d-inline lead">Chuck DeBrosse</p>
-            <p class="card-subtitle text-muted">Intern</p>
-            <p class="card-text">ID: 333</p>
-            <p class="card-text">School: OSU</p>
-            <p class="card-text">Email: <a href="mailto:cmdebrosse@outlook.com" class="card-link">cmdebrosse@outlook.com</a></p>
-          </div>
-        </div>
-  
+      <div class="row justify-content-center card-group" id="team-cards">
+
+      ${teamCards}
+
       </div>
     </div>
   </body>
@@ -67,4 +114,6 @@ module.exports = HTMLData => {
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   
   </html>`
-}
+};
+
+module.exports = newHTML;
